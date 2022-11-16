b2d7612ea924c6a16a10a626f048cafda1b20453
ikasu6 <116690487+ikasu6@users.noreply.github.com>
2022-11-15 16:05:20 -0800

    Updated changes

--- Tictactoe/test.py
+++ Tictactoe/test.py
@@ -1,14 +1,30 @@
 import unittest
-from logic import check_winner
+from logic import make_empty_board, DisplayBoard, check_winner,play,P1vsP2,P1vsBot
 
-board=[['X','2','O'], ['X','5','O'], ['X','O','X']]
-win=check_winner(board,'X')
+board=[['O','O','O'], ['X','5','O'], ['X','O','X']]
+win=check_winner(board,'O')
 print(win)
 
-##Still working on this
-
-'''class TestLogic(unittest.TestCase):
-    def test_get_winner(self):
+# The test based on unittest module
+class TestTicTactoeBoard(unittest.TestCase):
+    def test_check_vertical(self):
         board=[['X','2','O'], ['X','5','O'], ['X','O','X']]
-        win= ()
-        self.assertEqual(logic.check_winner(board,'X'),)'''
+        self.assertEqual(check_winner(board,'X'), 'X', "X should win")
+
+    def test_check_horizontal(self):
+        board=[['O','O','O'], ['X','5','O'], ['7','X','X']]
+        self.assertEqual(check_winner(board,'O'), 'O', "O should win ")
+
+    def test_check_diagonal(self):
+        board=[['O','X','X'], ['X','O','6'], ['7','8','O']]
+        self.assertEqual(check_winner(board,'O'), 'O', "O should win ")   
+
+    def test_check_nowin(self):
+        board=[['O','X','X'], ['X','O','6'], ['7','O','9']]
+        self.assertEqual(check_winner(board,'O'), None, "Nobody wins Yet ")     
+         
+
+
+# run the test
+if __name__ == '__main__':
+    unittest.main()      
